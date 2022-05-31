69573a8b458f0347cc0d07d23730733b451cbc76
LeoDog896 <LeoDog896@hotmail.com>
2022-05-31 14:17:40 +0000

    Add connection and event system factory

--- src/lib/eventSystemFactory.ts
+++ src/lib/eventSystemFactory.ts
@@ -1,24 +1,22 @@
+type OptionalIfUndefined<T> = (T extends undefined ? [] : never) | [T];
+
 export interface EventSystem<
   K extends Record<string, unknown>
 > {
-  on<E extends string>(key: E, callback: (event: K[E]) => void): void;
-  trigger<E extends string>(key: E, value: K[E]): void;
-}
-
-type BaseEvents = {
-  connect: void,
-  disconnect: void,
-  error: Error
+  on<E extends keyof K>(key: E, callback: (...[event]: OptionalIfUndefined<K[E]>) => void): void;
+  trigger<E extends keyof K>(key: E, ...[value]: OptionalIfUndefined<K[E]>): void;
 }
 
 export function eventSystemFactory<
-  K extends Record<string, unknown> = BaseEvents
+  K extends Record<string, unknown> 
 >(): EventSystem<K> {
 
-  const listeners: { [key in keyof K]?: (event: K[key]) => void } = {};
+  const listeners: { 
+    [key in keyof K]?: (...[event]: OptionalIfUndefined<K[key]>) => void 
+  } = {};
 
   return {
     on: (key, callback) => listeners[key] = callback,
-    trigger: (key, value) => listeners[key]?.(value)
+    trigger: (key, ...value) => listeners[key]?.(...value)
   }
 }
--- src/lib/peerConnection.ts
+++ src/lib/peerConnection.ts
@@ -1,15 +1,24 @@
+import { eventSystemFactory, type EventSystem } from "./eventSystemFactory"
+
+type BaseEvents = {
+  connect: void,
+  disconnect: void,
+  error: Error
+}
+
 type PeerConnectionType = "offerer" | "answerer"
 
 type PeerConnection = {
   connection: RTCPeerConnection;
-  connect: (description: RTCSessionDescription) => Promise<void>
 } & ({
   type: Extract<PeerConnectionType, "offerer">,
   offer: RTCSessionDescriptionInit;
   description: RTCSessionDescription;
+  connect: (description: RTCSessionDescription) => Promise<void>;
 } | {
   type: Extract<PeerConnectionType, "answerer">
-})
+  connect: (description: RTCSessionDescription) => Promise<RTCSessionDescription>;
+}) & EventSystem<BaseEvents>
 
 interface PeerConnectionOptions {
   iceServers: RTCIceServer[] | string[];
@@ -36,24 +45,40 @@ export const createPeerConnection = async (type: PeerConnectionType, options: Pa
   const connection = new RTCPeerConnection({
     iceServers
   });
+  
+  const eventSystem = eventSystemFactory<BaseEvents>()
 
   if (type == "offerer") {
     const offer = await connection.createOffer()
     await connection.setLocalDescription(offer);
     const description = await waitForIceCandidates(connection);
 
+    connection.addEventListener("connectionstatechange", () => {
+      if (connection.connectionState === "connected") {
+        eventSystem.trigger("connect")
+      }
+    })
+
     return {
       type: "offerer",
       connection,
       offer,
       description,
-      connect: async (description) => void 0
+      connect: async (description) => {
+        await connection.setRemoteDescription(description)
+      },
+      ...eventSystem
     }
   }
 
   return {
     type: "answerer",
     connection,
-    connect: async (description) => void 0
+    connect: async (description) => {
+      await connection.setRemoteDescription(description)
+      await connection.setLocalDescription(await connection.createAnswer())
+      return await waitForIceCandidates(connection)
+    },
+    ...eventSystem
   }
 }
