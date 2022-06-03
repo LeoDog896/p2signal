bd5c15d510b83d17501fdd15b3bf3390dbcb29f7
LeoDog896 <LeoDog896@hotmail.com>
2022-06-03 16:23:07 +0000

    /

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div class="w-screen p-10 text-center bg-green-200">
   <h1 class="text-2xl">p2signal</h1>
 
-  <span><a href="/docs">Docs</a> | <a href="/examples">Examples</a></span>
+  <span><a href="docs">Docs</a> | <a href="examples">Examples</a></span>
 </div>
