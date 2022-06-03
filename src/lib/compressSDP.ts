854fef26e69c78315484e652f5611187777700a9
LeoDog896 <LeoDog896@hotmail.com>
2022-06-03 15:03:07 +0000

    Cleanup

--- src/lib/compressSDP.ts
+++ src/lib/compressSDP.ts
@@ -1,13 +1,17 @@
 const beginSDP = `v=0\r\n`
+const endSDP = "\r\na=mid:0\r\na=sctp-port:5000\r\na=max-message-size:262144\r\n"
 
 function reduceSDP(sdp: string): string {
   return sdp.split("\r\n").map(it => {
     if (it == "v=0") return ""
+    if (it == "a=max-message-size:262144") return ""
+    if (it == "a=sctp-port:5000") return ""
+    if (it == "a=mid:0") return ""
 
     if (it.includes("a=fingerprint")) {
       const result = it.match(/(?<=fingerprint:sha-256 )([0-Z:]+)/g)
       if (!result) return
-      return `a=f${result[0].replaceAll(":", "")}`
+      return `f=${result[0].replaceAll(":", "")}`
     }
     
     return it
@@ -16,8 +20,8 @@ function reduceSDP(sdp: string): string {
 
 function expandSDP(sdp: string): string {
   return beginSDP + sdp.split(/(?=\w=)/g).map(it => {
-    if (it.match(/a=f[0-Z]/)) {
-      const result = it.match(/(?<=f)([0-Z:]+)/g)
+    if (it.includes("f=")) {
+      const result = it.match(/([0-Z:]+)/g)
       if (!result) throw Error("Could not find fingerprint")
 
       const splitSTR = result[0].matchAll(/[0-9A-Z]{2}/g)
@@ -25,7 +29,7 @@ function expandSDP(sdp: string): string {
       return `a=fingerprint:sha-256 ${[...splitSTR].join(":")}`
     }
     return it
-  }).join("\r\n") + "\r\n"
+  }).join("\r\n") + endSDP
 }
 
 /* eslint-disable no-case-declarations */
